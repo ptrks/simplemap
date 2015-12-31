5eaf7aee082221e8f41225b15ffdcd9011f83bff
Patrick Servello <patrick.servello@gmail.com>
2015-12-30 20:19:12 -0600

    added additional exception handing on file write

--- simplemap/map.py
+++ simplemap/map.py
@@ -20,28 +20,31 @@ class Map(object):
 
 	def __init__(self, title, center, zoom=11, markers=None, html_template='basic.html', config_file='config.json'):
 
+		self._env = Environment(loader=FileSystemLoader(TEMPLATES_DIR), trim_blocks=True, undefined=SilentUndefined)
+
 		self.title = title
-		self.html_template = html_template
+		self.template = self._env.get_template(html_template)
 		self.center = center
 		self.config = config_file
 		self.markers = markers
-		self._env = Environment(loader=FileSystemLoader(TEMPLATES_DIR), trim_blocks=True, undefined=SilentUndefined)
-		self._template = self._env.get_template('basic.html')
 
-
+	
 	def set_config(self, config_file):
 
 		try:
 			with open(config_file, "r") as config:    
 				self._config = json.load(config)
 
-		except IOError, e:
-			print "Error, unable to open {0} config file.".format(self.config_file)
+		except IOError:
+			print "Error, unable to open {0} config file.".format(config_file)
 			sys.exit()
 
-		except KeyError, e:
-			print "Error, `api_entry` not found in {0} config file.".format(self.config_file)
+		except KeyError:
+			print "Error, `api_entry` not found in {0} config file.".format(config_file)
 			sys.exit()
+
+		except Exception:
+			print "An unknown error occured while attempting to read {0} config file.".format(config_file)
 
 
 	def get_config(self):
@@ -57,7 +60,7 @@ class Map(object):
 
 			self._markers = markers
 
-			
+
 	def get_markers(self):
 
 		return self._markers
@@ -68,32 +71,21 @@ class Map(object):
 	markers = property(get_markers, set_markers)
 
 
-
-
-
-
-
-
-
-	# def add_markers(self, markers):
-
-	# 	for i in markers:
-
-	# 		if len(i) == 2:
-	# 			i.insert(0,'')
-	# 	self._markers = markers
-
-
-		
-
-
-
-
 	def write(self, output_path):
 
-		html = self._template.render(map_title = self.title, center=self.center, markers=self._markers, api_key=self.config['api_key'])
-		with open(output_path, "w") as output:
-			output.write(html)
+		try:
+
+			html = self.template.render(map_title = self.title, center=self.center, markers=self._markers, api_key=self.config['api_key'])
+			with open(output_path, "w") as output:
+				output.write(html)
+
+		except IOError:
+			print "Error, unable to write {0}".format(output_path)
+			sys.exit()
+
+		except Exception:
+			print "Undefined error occured while writing generating {0}".format(output_path)
+			sys.exit(0)
 
 
 
--- test_simplemap.py
+++ test_simplemap.py
@@ -4,4 +4,4 @@ import simplemap
 example = simplemap.Map('Example Map', [51.503454, -0.119562], markers=[[ 51.503454, -0.119562],[51.499633, -0.124755 ]], config_file='dev_config.json')
 
 
-example.write('output.html')
+example.write('output4.html')
