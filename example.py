6ebd4dd64d588707d8ecac9f8558d6af1709a61d
HFulcher <fulcherhuw@gmail.com>
2016-01-02 16:56:39 +0000

    Revert "Custom text and restructure of markers"
    This reverts commit 26bd87f71a83acae828fe21516f08ddcc2746a8c.

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ map_title = 'Example Map'
 center_point  = [34.5124, -118.1111]
 #center_point = None
 
-gps_markers = [ [34.4563, -118.1241, ['Example Text', 'Test']], [34.6432,-118.1554], [34.5124, -118.1111, ['', '']]]
+gps_markers = [ ['Example text', 34.4563,-118.1241], [34.6432,-118.1554] ]
 
 example_map = simplemap.Map(map_title, markers=gps_markers)
 example_map.write('example.html')
