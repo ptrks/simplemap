02279ebe59ec1876d17916d53637b5855e960ab6
Tom Nicklin <t-nicklin@hotmail.co.uk>
2016-10-03 19:24:59 +0100

    Issue where hover text wasn't making it to the html file. The problem was that because python assigns by reference a copied variable was effecting the original. Issue should now be resolved.

--- example.py
+++ example.py
@@ -5,6 +5,11 @@ center_point  = [34.5124, -118.1111]
 #center_point = None
 
 gps_markers = [ ['Example text', 34.4563,-118.1241], [34.5235,-118.1245], [34.6432,-118.1554] ]
+# TIL that python asigns by reference. Meaning that even though you shift data around in new_gps
+# it will effect the original :|. To avoid this I could have done `new_gps = gps_makers[:]` which 
+# would have made a new list. The issue is gps_markers is a 2 dimensional array/list, so it was only
+# looking one level deep. So we need to go deeper morty.
+new_gps = [sublist[:] for sublist in gps_markers]
 
 # This is how I have it the following in my provate repo. Couldn't work out how to add it to map.py
 # It is also completley optioinal. Simply remove the 'points' argument in the example_map below.
@@ -13,7 +18,7 @@ def make_points(coords):
 	if(coords):
 		# Firstly we don't want the hover text.
 		for x in range(0, len(coords)):
-			if(len(coords[x]) > 2):
+			if(len(coords[x]) == 3):
 				coords[x].pop(0)
 		# List comprehensions are fun. Basically telling it the format I want and then it has its 
 		# own little for loop and then changes all elements. It's more obvious when you look at
@@ -22,6 +27,8 @@ def make_points(coords):
 		# Return the new list after the list comprehension.
 	return new_list
 
+new_gps = make_points(new_gps)
 
-example_map = simplemap.Map(map_title, markers=gps_markers, points=make_points(gps_markers))
+
+example_map = simplemap.Map(map_title, markers=gps_markers, points=new_gps)
 example_map.write('example.html')
